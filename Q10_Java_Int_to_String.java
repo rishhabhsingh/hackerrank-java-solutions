8614dc6159f5c24c610b52c8908884c0bddcc969
Rishabh Singh <singhrishabh2308@gmail.com>
2026-01-20 21:18:28 +0530

    Implement integer to string conversion in Java

---
+++ Q10_Java_Int_to_String.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] argh){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String string = String.valueOf(n);
+
+        if (n == Integer.parseInt(string)){
+            System.out.println("Good job");
+        }
+        else {
+            System.out.println("Wrong answer");
+        }
+    }
+}
